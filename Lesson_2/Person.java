8b8e744df1e3be5f806c995322e9ce01c6663bfc
Yerzat Smagulov <smagulovyerzat@gmail.com>
2022-01-22 12:14:23 +0300

    Class

---
+++ Lesson_2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+    String humanName = "Сайтама";
+    char male = 'M';
+    int age = 25;
+    float humanMass = 65.8f;
+    float height = 172.3f;
+
+    void humanMove() {
+        System.out.println("Человек начал идти");
+    }
+
+    String humanSay() {
+        return "Я есть грут"
+    }
+
+    void humanSit() {
+        System.out.println("Сайтама сидит");
+    }
+
+    void humanRun() {
+        System.out.println("Сайтама начал бежать");
+    }
+
+    void learnJava() {
+        System.out.println("Сайтама начал изучать");
+    }
+}
+
